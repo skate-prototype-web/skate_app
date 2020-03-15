1bc02a6640c42056560e752d44458edd4f436fda
Scott Nassau <snassau@gmail.com>
2020-03-14 23:34:00 -0700

    create webpack

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+class Home extends Component {
+  constructor () {
+    super ()
+    this.state = {
+    }
+  }
+render() {
+    return (
+      <div> Hello from React
+      </div>
+    );
+  }
+}
+
+export default TourneyMainPage;
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`connected to port ${port}!`))
